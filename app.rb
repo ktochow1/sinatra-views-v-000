b6f6a8d09cd3b1c098cd1fd882a49adeb7731a8a
Kat Tochowicz <ktochow1@binghamton.edu>
2019-04-21 08:49:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,6 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	# get '/' do
-	# 	"<h1>Hello World</h1>"
-	# end
-
 	get '/' do
       erb :index
     end
@@ -13,17 +9,6 @@ class App < Sinatra::Base
 	get '/info' do
 		erb :info
 	end
-
-
-# end
-# =======
-#   erb :index
-#   end
-
-# 	get "/info" do
-# 		erb :info
-# 	end
 	
 	
 end 
-# >>>>>>> 45d0158a8497990c8088dcfa7383c890b64c2507
